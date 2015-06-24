4e50d13775194bf0c95cd3dc67b3f517c138e985
sharing <huangsharing@163.com>
2015-06-24 15:06:50 +0800

    测试提交

--- src/com/sharing/adttext/MainActivity.java
+++ src/com/sharing/adttext/MainActivity.java
@@ -2,6 +2,7 @@ package com.sharing.adttext;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.v("sharing", "测试的");
 	}
 
 	@Override
