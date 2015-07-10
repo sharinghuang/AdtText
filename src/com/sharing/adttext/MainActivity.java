990084035e65caabdb25cfb6a8e4ae39824a1d28
sharing <huangsharing@163.com>
2015-07-10 16:07:34 +0800

    SSS

--- src/com/sharing/adttext/MainActivity.java
+++ src/com/sharing/adttext/MainActivity.java
@@ -4,13 +4,31 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	TextView text;
+	Button btnText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		text = (TextView) findViewById(R.id.text);
+		btnText=(Button) findViewById(R.id.btn_text);
+		text.setText("my god");
+		btnText.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+
 		Log.v("sharing", "测试数据提交");
 	}
 
---
+++ src/com/sharing/adttext/MyApplication.java
@@ -0,0 +1,27 @@
+package com.sharing.adttext;
+
+import android.app.Application;
+
+public class MyApplication extends Application{
+	
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+	
+	/**
+	 * miaoshu 
+	 * @param ff
+	 * @param bb
+	 * @return 
+	 */
+	private int  add(int ff,int bb){
+		int result=1;
+		
+		return result;
+		
+	}
+		
+
+}
